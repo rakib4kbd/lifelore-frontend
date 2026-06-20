021deec544af61fab1a2976e734426eda0e62293
Rakib <rakib4kbd@gmail.com>
2026-06-20 17:41:55 +0600

    added admin layout and related pages

--- app/admin/overview/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DashboardAdminOverviewPage = () => {
-  return <div>DashboardAdminOverviewPage</div>;
-};
-
-export default DashboardAdminOverviewPage;
--- app/admin/settings/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminDashboardSettings = () => {
-  return <div>AdminDashboardSettings</div>;
-};
-
-export default AdminDashboardSettings;
